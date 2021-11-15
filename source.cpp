b301dc01eed4285358635dd440cdc9ce3311936b
User1 <dh51804453@student.stu.edu.vn>
2021-11-15 09:37:05 +0700

    Init file

---
+++ source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int hieu(int a, int b);
+int tong(int a, int b);
+int tich(int a, int b);
+double thuong(int a, int b);
+
+int main()
+{
+	int a, b;
+	cout << "Nhap so nguyen a: ";
+	cin >> a;
+	cout << "Nhap so nguyen b: ";
+	cin >> b;
+	cout << "KQ: ";
+	system("pause");
+	return 0;
+}
