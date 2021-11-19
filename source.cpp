c0494fbe28571f0798e1bcd65a0db38fca85e29a
haiduong03 <64822757+haiduong03@users.noreply.github.com>
2021-11-19 16:47:38 +0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int tong(int a, int b){return a+b;}
 int hieu(int a, int b){return a - b;}
-int tich(int a, int b){return a * b;}
+int tich(int a, int b);
 double thuong(int a, int b);
 
 int main()
@@ -15,7 +15,6 @@ int main()
 	cin >> b;
 	cout << "KQ: "<<tong(a,b);
 	cout << "KQ: "<<hieu(a,b);
-	cout << "KQ: "<<tich(a,b);
 	system("pause");
 	return 0;
 }
