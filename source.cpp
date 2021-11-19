d11871954639cc1956da9f302851036d143d2566
user2 <dh51804453@student.stu.edu.vn>
2021-11-19 17:35:14 +0700

    chanle

--- source.cpp
+++ source.cpp
@@ -5,6 +5,7 @@ int tong(int a, int b){return a+b;}
 int hieu(int a, int b){return a - b;}
 int tich(int a, int b){return a * b;}
 double thuong(int a, int b);
+bool chanle(int a){if(a%2==0} return 1; return 0;}
 
 int main()
 {
@@ -16,6 +17,8 @@ int main()
 	cout << "KQ: "<<tong(a,b);
 	cout << "KQ: "<<hieu(a,b);
 	cout << "KQ: "<<tich(a,b);
+	if(chanle(a)==1) cout<< "chan";
+	else cout << "le";
 	system("pause");
 	return 0;
 }
